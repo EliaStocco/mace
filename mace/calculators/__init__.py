b90dc0a8111df50cb2d8783e347a01496e3bb6a6
EliaStocco <eliastocco@gmail.com>
2024-03-26 15:16:53 +0100

    modified '__init__.py'

--- mace/calculators/__init__.py
+++ mace/calculators/__init__.py
@@ -1,14 +1,14 @@
 from .foundations_models import mace_anicc, mace_mp, mace_off
 from .lammps_mace import LAMMPS_MACE
 from .mace import MACECalculator
-from .macelia import MACEliaCalculator, MACEliaBECCalculator
+from .macelia import MACEliaCalculator
 
-__all__ = [
-    "MACECalculator",
-    "MACEliaCalculator",
-    "MACEliaBECCalculator",
-    "LAMMPS_MACE",
-    "mace_mp",
-    "mace_off",
-    "mace_anicc",
-]
+# __all__ = [
+#     "MACECalculator",
+#     "MACEliaCalculator",
+#     "MACEliaBECCalculator",
+#     "LAMMPS_MACE",
+#     "mace_mp",
+#     "mace_off",
+#     "mace_anicc",
+# ]
