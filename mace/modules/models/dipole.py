e55552b0d976af4e4d0633fb8ef981853e3ef7f2
EliaStocco <eliastocco@gmail.com>
2024-04-05 11:00:54 +0200

    created class 'AtomicDipolesMACE_MTP'

--- mace/modules/models/dipole.py
+++ mace/modules/models/dipole.py
@@ -63,7 +63,16 @@ def compute_dipole_from_point_charges(charges:torch.Tensor,data:Dict[str, torch.
     )
     return dipole, charges
 
-
+def compute_fixed_charge_dipole_same_unit(
+    charges: torch.Tensor,
+    positions: torch.Tensor,
+    batch: torch.Tensor,
+    num_graphs: int,
+) -> torch.Tensor:
+    mu = positions * charges.unsqueeze(-1)
+    return scatter_sum(
+        src=mu, index=batch.unsqueeze(-1), dim=0, dim_size=num_graphs
+    )  # [N_graphs,3]
 
 
 
@@ -499,117 +508,117 @@ class AtomicDipolesMACElia(BaseDipoleClass):
         # self._num_graphs = num_graphs
         return output
 
-class simpleshift(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.par = torch.nn.Parameter(torch.rand(3),requires_grad=True)
-    def forward(self):
-        return self.par
+# class simpleshift(torch.nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.par = torch.nn.Parameter(torch.rand(3),requires_grad=True)
+#     def forward(self):
+#         return self.par
     
-@compile_mode("script")
-class AtomicDipolesShiftMACE(AtomicDipolesMACElia):
+# @compile_mode("script")
+# class AtomicDipolesShiftMACE(AtomicDipolesMACElia):
 
-    def __init__(self, **kwargs):
-        # self._es_shift = o3.Linear(irreps_in="3x1o", irreps_out="1x1o")
-        super().__init__(**kwargs)
-        self.es_shift = simpleshift()#o3.Linear("","1x1o")# torch.nn.Parameter(torch.rand(1),requires_grad=True)
-        self.products.append(self.es_shift)
+#     def __init__(self, **kwargs):
+#         # self._es_shift = o3.Linear(irreps_in="3x1o", irreps_out="1x1o")
+#         super().__init__(**kwargs)
+#         self.es_shift = simpleshift()#o3.Linear("","1x1o")# torch.nn.Parameter(torch.rand(1),requires_grad=True)
+#         self.products.append(self.es_shift)
 
-    def forward(
-        self,
-        data: Dict[str, torch.Tensor],
-        training: bool = False,  # pylint: disable=W0613
-        compute_force: bool = False,
-        compute_virials: bool = False,
-        compute_stress: bool = False,
-        compute_displacement: bool = False,
-    ) -> Dict[str, Optional[torch.Tensor]]:
-        output = super().forward(
-            data,
-            training,
-            compute_force,
-            compute_virials,
-            compute_stress,
-            compute_displacement,
-        )
-        output["dipole"] = output["dipole"] + self.es_shift()
-        return output
+#     def forward(
+#         self,
+#         data: Dict[str, torch.Tensor],
+#         training: bool = False,  # pylint: disable=W0613
+#         compute_force: bool = False,
+#         compute_virials: bool = False,
+#         compute_stress: bool = False,
+#         compute_displacement: bool = False,
+#     ) -> Dict[str, Optional[torch.Tensor]]:
+#         output = super().forward(
+#             data,
+#             training,
+#             compute_force,
+#             compute_virials,
+#             compute_stress,
+#             compute_displacement,
+#         )
+#         output["dipole"] = output["dipole"] + self.es_shift()
+#         return output
 
-@compile_mode("script")
-class AtomicDipolesMACEliaBias(AtomicDipolesMACElia):
-    """Dipole with the Modr Theory of Polarization + fixed charges contribution."""
+# @compile_mode("script")
+# class AtomicDipolesMACEliaBias(AtomicDipolesMACElia):
+#     """Dipole with the Modr Theory of Polarization + fixed charges contribution."""
 
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        # self.mtp      = o3.Linear("1x0e + 1x1o","1x1o")
-        self.mtp_shift = o3.Linear("1x1o","1x1o")
-        self.products.append(self.mtp_shift)
+#     def __init__(self,**kwargs):
+#         super().__init__(**kwargs)
+#         # self.mtp      = o3.Linear("1x0e + 1x1o","1x1o")
+#         self.mtp_shift = o3.Linear("1x1o","1x1o")
+#         self.products.append(self.mtp_shift)
 
-    def forward(
-        self,
-        data: Dict[str, torch.Tensor],
-        training: bool = False,  # pylint: disable=W0613
-        compute_force: bool = False,
-        compute_virials: bool = False,
-        compute_stress: bool = False,
-        compute_displacement: bool = False,
-    ) -> Dict[str, Optional[torch.Tensor]]:
-        output = super().forward(
-            data,
-            training,
-            compute_force,
-            compute_virials,
-            compute_stress,
-            compute_displacement,
-        )
-        # a = torch.column_stack((self._charges.unsqueeze(1),data["delta_positions"]))
-        # broke_transl_sym = self.mtp(a)
-        # broke_transl_sym = scatter_sum(
-        #     src=broke_transl_sym,
-        #     index=data["batch"],
-        #     dim=0,
-        #     dim_size=self._num_graphs,
-        # )
+#     def forward(
+#         self,
+#         data: Dict[str, torch.Tensor],
+#         training: bool = False,  # pylint: disable=W0613
+#         compute_force: bool = False,
+#         compute_virials: bool = False,
+#         compute_stress: bool = False,
+#         compute_displacement: bool = False,
+#     ) -> Dict[str, Optional[torch.Tensor]]:
+#         output = super().forward(
+#             data,
+#             training,
+#             compute_force,
+#             compute_virials,
+#             compute_stress,
+#             compute_displacement,
+#         )
+#         # a = torch.column_stack((self._charges.unsqueeze(1),data["delta_positions"]))
+#         # broke_transl_sym = self.mtp(a)
+#         # broke_transl_sym = scatter_sum(
+#         #     src=broke_transl_sym,
+#         #     index=data["batch"],
+#         #     dim=0,
+#         #     dim_size=self._num_graphs,
+#         # )
 
-        output["dipole"] = output["dipole"] + self.mtp_shift(output["dipole"])
+#         output["dipole"] = output["dipole"] + self.mtp_shift(output["dipole"])
 
-        return output
+#         return output
 
 
-@compile_mode("script")
-class AtomicDipolesMACEliaMTP(AtomicDipolesMACElia):
-    """Dipole with the Modern Theory of Polarization + fixed charges contribution."""
+# @compile_mode("script")
+# class AtomicDipolesMACEliaMTP(AtomicDipolesMACElia):
+#     """Dipole with the Modern Theory of Polarization + fixed charges contribution."""
 
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        self.jump      = o3.Linear("3x1o","1x1o")
-        self.products.append(self.jump)
-        pass
+#     def __init__(self,**kwargs):
+#         super().__init__(**kwargs)
+#         self.jump      = o3.Linear("3x1o","1x1o")
+#         self.products.append(self.jump)
+#         pass
 
-    def forward(
-        self,
-        data: Dict[str, torch.Tensor],
-        training: bool = False,  # pylint: disable=W0613
-        compute_force: bool = False,
-        compute_virials: bool = False,
-        compute_stress: bool = False,
-        compute_displacement: bool = False,
-    ) -> Dict[str, Optional[torch.Tensor]]:
-        output = super().forward(
-            data,
-            training,
-            compute_force,
-            compute_virials,
-            compute_stress,
-            compute_displacement,
-        )
+#     def forward(
+#         self,
+#         data: Dict[str, torch.Tensor],
+#         training: bool = False,  # pylint: disable=W0613
+#         compute_force: bool = False,
+#         compute_virials: bool = False,
+#         compute_stress: bool = False,
+#         compute_displacement: bool = False,
+#     ) -> Dict[str, Optional[torch.Tensor]]:
+#         output = super().forward(
+#             data,
+#             training,
+#             compute_force,
+#             compute_virials,
+#             compute_stress,
+#             compute_displacement,
+#         )
 
-        jump = self.jump(data["cell"].reshape((-1,9)))
-        # jump[:,0] = 0
-        # jump[:,1] = 0
-        output["dipole"] = output["dipole"] + jump/100.
+#         jump = self.jump(data["cell"].reshape((-1,9)))
+#         # jump[:,0] = 0
+#         # jump[:,1] = 0
+#         output["dipole"] = output["dipole"] + jump/100.
 
-        return output
+#         return output
 
 @compile_mode("script")
 class DipolesPointCharges(BaseDipoleClass):
@@ -810,3 +819,204 @@ class DipolesPointCharges(BaseDipoleClass):
         }
         return output
     
+@compile_mode("script")
+class AtomicDipolesMACE_MTP(BaseDipoleClass):
+    
+    def __init__(
+        self,
+        r_max: float,
+        num_bessel: int,
+        num_polynomial_cutoff: int,
+        max_ell: int,
+        interaction_cls: Type[InteractionBlock],
+        interaction_cls_first: Type[InteractionBlock],
+        num_interactions: int,
+        num_elements: int,
+        hidden_irreps: o3.Irreps,
+        MLP_irreps: o3.Irreps,
+        avg_num_neighbors: float,
+        atomic_numbers: List[int],
+        correlation: int,
+        gate: Optional[Callable],
+        atomic_energies: Optional[
+            None
+        ],  # Just here to make it compatible with energy models, MUST be None
+        radial_type: Optional[str] = "bessel",
+        radial_MLP: Optional[List[int]] = None,
+    ):
+        super().__init__()
+        self.register_buffer(
+            "atomic_numbers", torch.tensor(atomic_numbers, dtype=torch.int64)
+        )
+        self.register_buffer("r_max", torch.tensor(r_max, dtype=torch.float64))
+        self.register_buffer(
+            "num_interactions", torch.tensor(num_interactions, dtype=torch.int64)
+        )
+        assert atomic_energies is None
+
+        # Embedding
+        node_attr_irreps = o3.Irreps([(num_elements, (0, 1))])
+        node_feats_irreps = o3.Irreps([(hidden_irreps.count(o3.Irrep(0, 1)), (0, 1))])
+        self.node_embedding = LinearNodeEmbeddingBlock(
+            irreps_in=node_attr_irreps, irreps_out=node_feats_irreps
+        )
+        self.radial_embedding = RadialEmbeddingBlock(
+            r_max=r_max,
+            num_bessel=num_bessel,
+            num_polynomial_cutoff=num_polynomial_cutoff,
+            radial_type=radial_type,
+        )
+        edge_feats_irreps = o3.Irreps(f"{self.radial_embedding.out_dim}x0e")
+
+        sh_irreps = o3.Irreps.spherical_harmonics(max_ell)
+        num_features = hidden_irreps.count(o3.Irrep(0, 1))
+        interaction_irreps = (sh_irreps * num_features).sort()[0].simplify()
+        self.spherical_harmonics = o3.SphericalHarmonics(
+            sh_irreps, normalize=True, normalization="component"
+        )
+        if radial_MLP is None:
+            radial_MLP = [64, 64, 64]
+
+        # Interactions and readouts
+        inter = interaction_cls_first(
+            node_attrs_irreps=node_attr_irreps,
+            node_feats_irreps=node_feats_irreps,
+            edge_attrs_irreps=sh_irreps,
+            edge_feats_irreps=edge_feats_irreps,
+            target_irreps=interaction_irreps,
+            hidden_irreps=hidden_irreps,
+            avg_num_neighbors=avg_num_neighbors,
+            radial_MLP=radial_MLP,
+        )
+        self.interactions = torch.nn.ModuleList([inter])
+
+        # Use the appropriate self connection at the first layer
+        use_sc_first = False
+        if "Residual" in str(interaction_cls_first):
+            use_sc_first = True
+
+        node_feats_irreps_out = inter.target_irreps
+        prod = EquivariantProductBasisBlock(
+            node_feats_irreps=node_feats_irreps_out,
+            target_irreps=hidden_irreps,
+            correlation=correlation,
+            num_elements=num_elements,
+            use_sc=use_sc_first,
+        )
+        self.products = torch.nn.ModuleList([prod])
+
+        self.readouts = torch.nn.ModuleList()
+        self.readouts.append(LinearDipoleReadoutBlock(hidden_irreps, dipole_only=True))
+
+        for i in range(num_interactions - 1):
+            if i == num_interactions - 2:
+                assert (
+                    len(hidden_irreps) > 1
+                ), "To predict dipoles use at least l=1 hidden_irreps"
+                hidden_irreps_out = str(
+                    hidden_irreps[1]
+                )  # Select only l=1 vectors for last layer
+            else:
+                hidden_irreps_out = hidden_irreps
+            inter = interaction_cls(
+                node_attrs_irreps=node_attr_irreps,
+                node_feats_irreps=hidden_irreps,
+                edge_attrs_irreps=sh_irreps,
+                edge_feats_irreps=edge_feats_irreps,
+                target_irreps=interaction_irreps,
+                hidden_irreps=hidden_irreps_out,
+                avg_num_neighbors=avg_num_neighbors,
+                radial_MLP=radial_MLP,
+            )
+            self.interactions.append(inter)
+            prod = EquivariantProductBasisBlock(
+                node_feats_irreps=interaction_irreps,
+                target_irreps=hidden_irreps_out,
+                correlation=correlation,
+                num_elements=num_elements,
+                use_sc=True,
+            )
+            self.products.append(prod)
+            if i == num_interactions - 2:
+                self.readouts.append(
+                    NonLinearDipoleReadoutBlock(
+                        hidden_irreps_out, MLP_irreps, gate, dipole_only=True
+                    )
+                )
+            else:
+                self.readouts.append(
+                    LinearDipoleReadoutBlock(hidden_irreps, dipole_only=True)
+                )
+
+    def forward(
+        self,
+        data: Dict[str, torch.Tensor],
+        training: bool = False,  # pylint: disable=W0613
+        compute_force: bool = False,
+        compute_virials: bool = False,
+        compute_stress: bool = False,
+        compute_displacement: bool = False,
+    ) -> Dict[str, Optional[torch.Tensor]]:
+        assert compute_force is False
+        assert compute_virials is False
+        assert compute_stress is False
+        assert compute_displacement is False
+        # Setup
+        data["node_attrs"].requires_grad_(True)
+        data["positions"].requires_grad_(True)
+        num_graphs = data["ptr"].numel() - 1
+
+        # Embeddings
+        node_feats = self.node_embedding(data["node_attrs"])
+        vectors, lengths = get_edge_vectors_and_lengths(
+            positions=data["positions"],
+            edge_index=data["edge_index"],
+            shifts=data["shifts"],
+        )
+        edge_attrs = self.spherical_harmonics(vectors)
+        edge_feats = self.radial_embedding(lengths)
+
+        # Interactions
+        dipoles = []
+        for interaction, product, readout in zip(
+            self.interactions, self.products, self.readouts
+        ):
+            node_feats, sc = interaction(
+                node_attrs=data["node_attrs"],
+                node_feats=node_feats,
+                edge_attrs=edge_attrs,
+                edge_feats=edge_feats,
+                edge_index=data["edge_index"],
+            )
+            node_feats = product(
+                node_feats=node_feats,
+                sc=sc,
+                node_attrs=data["node_attrs"],
+            )
+            node_dipoles = readout(node_feats).squeeze(-1)  # [n_nodes,3]
+            dipoles.append(node_dipoles)
+
+        # Compute the dipoles
+        contributions_dipoles = torch.stack(
+            dipoles, dim=-1
+        )  # [n_nodes,3,n_contributions]
+        atomic_dipoles = torch.sum(contributions_dipoles, dim=-1)  # [n_nodes,3]
+        total_dipole = scatter_sum(
+            src=atomic_dipoles,
+            index=data["batch"],
+            dim=0,
+            dim_size=num_graphs,
+        )  # [n_graphs,3]
+        baseline = compute_fixed_charge_dipole_same_unit(
+            charges=data["charges"],
+            positions=data["positions"],
+            batch=data["batch"],
+            num_graphs=num_graphs,
+        )  # [n_graphs,3]
+        total_dipole = total_dipole + baseline
+
+        output = {
+            "dipole": total_dipole,
+            "atomic_dipoles": atomic_dipoles,
+        }
+        return output
